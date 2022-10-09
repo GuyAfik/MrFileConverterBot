bee2c121bacde48f75f590029dbf2b8a2df5cd26
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-10-09 23:37:47 +0300

    pre-commit fixes (#7)

--- mr_file_converter/command/command_service.py
+++ mr_file_converter/command/command_service.py
@@ -8,11 +8,13 @@ class CommandService:
     """
     This service provides the interface for the basic commands that this bot supports such as start, cancel, help
     """
+
     def __init__(self, telegram_service: TelegramService):
         self.telegram_service = telegram_service
 
     def start(self, update: Update, context: CallbackContext):
-        first_name, last_name = self.telegram_service.get_user_first_and_last_name(update)
+        first_name, last_name = self.telegram_service.get_user_first_and_last_name(
+            update)
         self.telegram_service.send_message(
             update=update, text=f'Welcome {first_name} {last_name}, I am '
                                 f'available commands:\n/get_youtube_video: Get youtube video in mp3/mp4 format.'
