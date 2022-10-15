2ef42ce7f2ee604e8e179b5235c3e51843c23ad6
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-10-15 23:28:51 +0300

    add support to convert xml to json/yml (#21)
    * add support to convert xml to json/yml
    
    * pre-commit fixes

--- mr_file_converter/containers.py
+++ mr_file_converter/containers.py
@@ -15,6 +15,7 @@ from mr_file_converter.file.file_service import FileService
 from mr_file_converter.io.io_service import IOService
 from mr_file_converter.json.json_service import JsonService
 from mr_file_converter.telegram.telegram_service import TelegramService
+from mr_file_converter.xml.xml_service import XMLService
 from mr_file_converter.yaml.yaml_service import YamlService
 
 
@@ -58,13 +59,22 @@ class Services(containers.DeclarativeContainer):
         yml_converter=converters.yaml,
         xml_converter=converters.xml
     )
+    xml = providers.Factory(
+        XMLService,
+        command_service=command,
+        io_service=io,
+        json_converter=converters.json,
+        yml_converter=converters.yaml,
+        xml_converter=converters.xml
+    )
     file = providers.Factory(
         FileService,
         telegram_service=telegram,
         io_service=io,
         command_service=command,
         json_service=json,
-        yaml_service=yaml
+        yaml_service=yaml,
+        xml_service=xml
     )
 
 
