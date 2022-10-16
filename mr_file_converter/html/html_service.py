429207d17932332002eae7df451ed71a99e14935
GuyAfik <guyafik11@gmail.com>
2022-10-16 12:49:53 +0300

    pre-commit fixes

--- mr_file_converter/html/html_service.py
+++ mr_file_converter/html/html_service.py
@@ -1,8 +1,9 @@
 import os
 from contextlib import contextmanager
 from typing import Generator
-from html2image import Html2Image
+
 import pdfkit
+from html2image import Html2Image
 
 from mr_file_converter.command.command_service import CommandService
 from mr_file_converter.io.io_service import IOService
@@ -13,6 +14,7 @@ class HTMLService:
     TODO: https://github.com/JazzCore/python-pdfkit - need to install wkhtmltopdf on the docker
     TODO: brew install homebrew/cask/wkhtmltopdf
     """
+
     def __init__(
         self,
         command_service: CommandService,
@@ -35,5 +37,6 @@ class HTMLService:
             prefix=custom_file_name or os.path.splitext(source_file_path)[0]
         ) as png_file:
             html_to_image = Html2Image()
-            html_to_image.screenshot(html_file=source_file_path, save_as=png_file)
+            html_to_image.screenshot(
+                html_file=source_file_path, save_as=png_file)
             yield png_file
