301f275aeadeab46c980077a791506a5fe122971
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-10-19 20:59:58 +0300

    add command service unit-tests (#39)

--- mr_file_converter/file/errors.py
+++ mr_file_converter/file/errors.py
@@ -3,7 +3,7 @@ from mr_file_converter.base_error import FileConverterException
 
 class FileTypeNotSupported(FileConverterException):
 
-    def __init__(self, next_stage: int, _file_type: str):
+    def __init__(self, _file_type: str, next_stage: int | None = None):
         super().__init__(
             next_stage=next_stage,
             error_message=f'file is of type {_file_type} and is not supported at the moment'
@@ -12,7 +12,7 @@ class FileTypeNotSupported(FileConverterException):
 
 class FileConversionError(FileConverterException):
 
-    def __init__(self, next_stage: int, source_format: str, target_format: str):
+    def __init__(self, source_format: str, target_format: str, next_stage: int | None = None):
         super().__init__(
             next_stage=next_stage,
             error_message=f'Error when converting from source format {source_format} to target format {target_format}'
