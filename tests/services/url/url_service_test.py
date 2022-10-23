7ec78c46586ca8d0a29d23cf01c82c639d6eb2bd
GuyAfik <guyafik11@gmail.com>
2022-10-23 23:19:47 +0300

    add unit-test

--- tests/services/url/url_service_test.py
+++ tests/services/url/url_service_test.py
@@ -58,6 +58,26 @@ def test_url_to_png(url_service: URLService):
     """
     with url_service.to_png(
         url='https://www.google.com/', custom_file_name='test'
-    ) as html_file:
-        assert os.path.exists(html_file)
-        assert html_file == 'test.png'
+    ) as png_file:
+        assert os.path.exists(png_file)
+        assert png_file == 'test.png'
+
+
+def test_url_to_jpg(url_service: URLService):
+    """
+    Given:
+     - google URL.
+     - custom file name.
+
+    When:
+     - converting url into a png file
+
+    Then:
+     - make sure the file creation succeeds and that the file exists.
+     - make sure the name of the file is the correct.
+    """
+    with url_service.to_jpg(
+        url='https://www.google.com/', custom_file_name='test'
+    ) as jpg_file:
+        assert os.path.exists(jpg_file)
+        assert jpg_file == 'test.jpg'
