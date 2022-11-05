3dcad61d8a1e8bdb76d2ba39b1bf4fb5fb53f41f
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-11-05 16:23:41 +0200

    cosmetic code changes (#85)

--- mr_file_converter/services/io/io_service.py
+++ mr_file_converter/services/io/io_service.py
@@ -58,11 +58,8 @@ class IOService:
 
     @contextmanager
     def create_temp_png_file(self, prefix: str) -> Generator[str, None, None]:
-        try:
-            with self.create_temp_file(prefix=prefix, suffix=".png") as out_path:
-                yield out_path
-        finally:
-            pass
+        with self.create_temp_file(prefix=prefix, suffix=".png") as out_path:
+            yield out_path
 
     @contextmanager
     def create_temp_jpg_file(self, prefix: str) -> Generator[str, None, None]:
